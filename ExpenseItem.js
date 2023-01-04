427e20e852ef48f267be80d13d465cb737f56fd1
sravanisravs1 <108719334+sravanisravs1@users.noreply.github.com>
2023-01-04 12:14:35 +0530

    Initial Commit
    Added Expense items Using React component

---
+++ ExpenseItem.js
@@ -0,0 +1,16 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <h2>Expense Items</h2>
+            <ul>
+                 <li>Food Rs10</li>
+                 <li>Petrol Rs100</li>
+                 <li>Movies Rs200</li>
+            </ul>
+        </div>
+    );    
+    
+
+}
+
+export default ExpenseItem;
