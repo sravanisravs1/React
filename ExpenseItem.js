404e3f19cb4338cd81548aa309774803b4411667
sravanisravs1 <108719334+sravanisravs1@users.noreply.github.com>
2023-01-05 10:27:52 +0530

    Basic coding using Variables
    Added Variables for saving data in JSX code

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,12 +1,21 @@
+import "./ExpenseItem.css"
+
 function ExpenseItem(){
+
+    const expenseItem="Car Insurance";
+    const expenseDate=new Date(2020,0,5);
+    const expenseAmount=300;
+    const locationOfExpenditure='Mumbai';
     return (
-        <div>
-            <h2>Expense Items</h2>
-            <ul>
-                 <li>Food Rs10</li>
-                 <li>Petrol Rs100</li>
-                 <li>Movies Rs200</li>
-            </ul>
+        <div className="expense-item">
+            <div >{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                 <h2>{expenseItem}</h2>
+                 <div>Spent in {locationOfExpenditure}</div>
+                 <div className="expense-item__price">Rs{expenseAmount}</div>
+
+            </div>
+            
         </div>
     );    
     
