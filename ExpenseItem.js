1f2eff85084295dbf8153706b8bae263c63bfe49
sravanisravs1 <108719334+sravanisravs1@users.noreply.github.com>
2023-01-09 20:54:50 +0530

    Added Buttons
    Adding event listeners and adding functions to them

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -6,12 +6,19 @@ import React from "react";
 
 const ExpenseItem = (props) => {
     
-    
+    const clickHandler=()=>{
+        console.log("Updated")
+
+    }
+    const deleteTitle=()=>{
+        console.log("Deleted")
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}> </ExpenseDate>
             <ExpenseDetails title={props.title} locationOfExpenditure={props.locationOfExpenditure} amount={props.amount} />
-            
+            <button onClick={clickHandler}>Change Title</button>
+            <button onClick={deleteTitle}></button>
         </Card>
     );    
 }
