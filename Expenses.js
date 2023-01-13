f30685107ba1a549e656ffbf9462f6344f73e541
sravanisravs1 <108719334+sravanisravs1@users.noreply.github.com>
2023-01-13 22:01:20 +0530

    Added expenses using map
    TO change the expenses added dynamically,we use array.map function

--- Expenses.js
+++ Expenses.js
@@ -13,27 +13,16 @@ const Expenses = (props) => {
     <div>
         <Card className="expenses">
           <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-          <ExpenseItem
-            title={props.items[0].title}
-            amount={props.items[0].amount}
-            date={props.items[0].date}
-          />
-          <ExpenseItem
-            title={props.items[1].title}
-            mount={props.items[1].amount}
-            date={props.items[1].date}
-          />
-          <ExpenseItem
-             title={props.items[2].title}
-             amount={props.items[2].amount}
-            date={props.items[2].date}
-          />
-          <ExpenseItem
-            title={props.items[3].title}
-            amount={props.items[3].amount}
-            date={props.items[3].date}
-          />
-    </Card>
+          {
+            props.items.map((expense)=>(
+              <ExpenseItem
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+                location={expense.locationOfExpenditure}/>
+            ))
+          }
+        </Card>
     </div>
 
   );
