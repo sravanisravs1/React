9db86bd0966202d0d0ef8c2bdd74c2876b56fb64
sravanisravs1 <108719334+sravanisravs1@users.noreply.github.com>
2023-01-15 21:22:07 +0530

    Added Functionality to filtercomponent
    Using conditional rendering,displayed the expenses based on selected year

--- Expenses.js
+++ Expenses.js
@@ -9,12 +9,23 @@ const Expenses = (props) => {
   const filterChangeHandler = selectedYear =>{
     setFilteredYear(selectedYear);
   } ;
+  const filteredExpenses= props.items.filter((expense) => {
+    return expense.date.getFullYear().toString()===filteredYear;
+  })
   return (
     <div>
         <Card className="expenses">
           <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-          {
+          {/* {
             props.items.map((expense)=>(
+              <ExpenseItem
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+                location={expense.locationOfExpenditure}/>
+            ))
+          } */}{
+            filteredExpenses.map((expense)=>(
               <ExpenseItem
                 title={expense.title}
                 amount={expense.amount}
