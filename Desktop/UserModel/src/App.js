3aff570dd001a0799d6546da7ac0214d326d3aa5
sravanisravs1 <108719334+sravanisravs1@users.noreply.github.com>
2023-01-21 15:22:25 +0530

    Updated CollegeName field
    Modified the form and Rendering list

--- Desktop/UserModel/src/App.js
+++ Desktop/UserModel/src/App.js
@@ -6,8 +6,8 @@ import UsersList from './Components/Users/UsersList';
 function App() {
   const [usersList,setUsersList] = useState([]);
 
-  const addUserHandler = (userName,userAge) =>{
-    setUsersList((prevUsersList) => [...prevUsersList,{name:userName,age:userAge,id: Math.random().toString()}])
+  const addUserHandler = (userName,userAge,collegeName) =>{
+    setUsersList((prevUsersList) => [...prevUsersList,{name:userName,age:userAge,collegeName:collegeName,id: Math.random().toString()}])
   }
 
   return (
