65b1382de40ac453f8b6e5316ae1efbba58f139e
sravanisravs1 <108719334+sravanisravs1@users.noreply.github.com>
2023-01-21 14:29:10 +0530

    Added Portals,Fragments
    Adding Portal for clean semantic HTML

--- Desktop/UserModel/src/Components/UI/ErrorModel/ErrorModel.js
+++ Desktop/UserModel/src/Components/UI/ErrorModel/ErrorModel.js
@@ -1,28 +1,49 @@
 import React  from "react";
+import ReactDom from 'react-dom';
 import Card from "../Card/Card";
 import Button from "../Button/Button";
-import classes from "./ErrorModel.module.css"
+import classes from "./ErrorModel.module.css";
 
+const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onConfirm}/>
+};
+
+const ModelOverlay=(props) =>{
+    return (<Card className={classes.modal}>
+        <header className={classes.header}>
+            <h2>{props.title}
+            </h2>
+        </header>
+        <div className={classes.content}>
+            <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+            <Button onClick={props.onClick}>Okay</Button>
+
+        </footer>
+    </Card>
+        
+    );
+};
 const ErrorModel= props =>{
     
-    return(<div>
-        <div className={classes.backdrop} onClick={props.onConfirm}>
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}
-                    </h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onClick}>Okay</Button>
+    return(<React.Fragment>
+        {ReactDom.createPortal(
+        <Backdrop 
+        onConfirm={props.onConfirm}/>,
+        document.getElementById('backdrop-root'))}
 
-                </footer>
-            </Card>
-        </div>
+        {ReactDom.createPortal(
+            <ModelOverlay 
+            title={props.title}
+            message={props.message}
+            onClick={props.onConfirm}
+            />,
+           document.getElementById('overlay-root') 
 
-    </div>) 
+        )}
+        
+    </React.Fragment>) 
 
 };
 
